27dc1804384daecc97a3ca343efe005b2ef10411
Ali-A-A-Musleh <44870616+Ali-A-A-Musleh@users.noreply.github.com>
2026-05-10 02:00:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MEAL-Pro-Analytics-v18/',
+  base: process.env.GITHUB_ACTIONS ? '/MEAL-Pro-Analytics-v18/' : '/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000
+  },
   build: {
     target: 'es2020',
     assetsInlineLimit: 1000000,
